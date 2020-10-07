8c0a7195838393d4df204914386914b746487c57
Gabriel Leal <gabrielthleal@gmail.com>
2020-10-07 12:19:59 -0300

    generate docs and some fixes

--- app/serializers/question_access_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class QuestionAccessSerializer < ActiveModel::Serializer
-  attributes :id, :question_id, :date, :times_accessed, :created_at, :updated_at
-
-  belongs_to :question
-end
