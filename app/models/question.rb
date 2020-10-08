d80944c47c80c0fb6e062b91dab6ada559841902
Gabriel Leal <gabrielthleal@gmail.com>
2020-10-08 17:28:48 -0300

    fixes typo

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ApplicationRecord
 
   scope :most_daily_accessed, -> { order(daily_access: :desc) }
 
-  scope :most_accessed_by_periode, lambda { |from, to|
+  scope :most_accessed_by_period, lambda { |from, to|
     joins(:question_access).where('date >= ? AND date <= ?', from, to)
                            .order(times_accessed: :desc)
   }
@@ -15,21 +15,21 @@ class Question < ApplicationRecord
       from = date.to_time.at_beginning_of_week
       to = date.to_time.at_end_of_week
 
-      most_accessed_by_periode(from, to)
+      most_accessed_by_period(from, to)
     end
 
     def most_accessed_by_month(date)
       from = date.to_time
       to = from.at_end_of_month
 
-      most_accessed_by_periode(from, to)
+      most_accessed_by_period(from, to)
     end
 
     def most_accessed_by_year(date)
       from = date.to_time
       to = from.at_end_of_year
 
-      most_accessed_by_periode(from, to)
+      most_accessed_by_period(from, to)
     end
   end
 end
